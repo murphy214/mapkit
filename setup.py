7efd00b42753ed8227d979ecdad222544aee97f3
Bennett Murphy <murphy214@live.marshall.edu>
2017-05-27 03:52:04 -0400

    pushing tarsal changes

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(name='mapkit',
       url='https://github.com/murphy214/mapkit',
       packages=['mapkit'],
       install_requires = [#'nlgeojson',
-      						'pipeleaflet',
-      						'pipevts',
-      						'pipegls',
+      						#'pipeleaflet',
+      						#'pipevts',
+      						#'pipegls',
       						'future',
       						'geopandas',
       						'numpy',
@@ -21,6 +21,6 @@ setup(name='mapkit',
       						],
 
       scripts = ['bin/myfreeport'],
-      dependency_links=['http://github.com/murphy214/nlgeojson/tarball/master#egg=package-1.0']
+      dependency_links=['http://github.com/murphy214/nlgeojson/tarball/master#egg=package-1.0','http://github.com/murphy214/pipeleaflet/tarball/master#egg=package-1.0','http://github.com/murphy214/pipevts/tarball/master#egg=package-1.0','http://github.com/murphy214/pipegls/tarball/master#egg=package-1.0']
 
       )
